2fc4d2b89cb077aa17f9c7142a71b37c81b40d88
Joseph Perez <hello@josephdanielperez.com>
2020-06-27 23:57:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array = [1,2,3])
+  new_array = []
   array.each do |number|
     number = number ** 2
-    array << number
+    new_array << number
   end
+  return new_array
 end
-
-square_array
